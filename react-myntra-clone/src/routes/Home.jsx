410fa07bffe75b763531866a531691341cb8835b
ThakrePiyush <piyushthakre098@gmail.com>
2024-10-18 19:21:31 +0530

    minor change

---
+++ react-myntra-clone/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import HomeItem from "../components/HomeItem";
+
+const Home = () => {
+ const items =  useSelector(Store=> Store.items)
+
+   return <>
+      <main>
+        <div className="items-container">
+            {items.map(item => <HomeItem key={item.id} item={item}></HomeItem> )}   
+        </div>
+    </main>
+   </>
+}
+
+export default Home;
